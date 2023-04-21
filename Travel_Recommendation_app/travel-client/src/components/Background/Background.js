35cd36d2f030e8d5a095525f5e4576bbc562b7bd
Nishi Pancholi <pancholi.n@northeastern.edu>
2023-04-20 21:58:09 -0400

    Background component

---
+++ Travel_Recommendation_app/travel-client/src/components/Background/Background.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import imagebg from "../../img/images/Miami.jpg";
+
+class Background extends Component {
+  render() {
+    return (
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide1"
+        data-bs-ride="carousel"
+      >
+
+      </div>
+    );
+  }
+}
+
+export default Background;
